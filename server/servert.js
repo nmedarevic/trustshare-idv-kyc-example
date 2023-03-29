e2a65fce9203c52a0571679f36da56616c1c3e01
Nikola Medarevic <nikola@petmediagroup.com>
2023-03-29 13:16:47 +0200

    Trustshare idv kyc example init

---
+++ server/servert.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const { default: createClient } = require('@trustshare/api');
+const app = express()
+const port = 4000
+
+const trustshare = createClient('sandbox_api_a1lVBwqwYFpdxQMrngeKC113dKxh4whnLeGT37GfdaV4629O6UfVn9OEk0PEZKwt');
+
+app.get('/get-verification', async (req, res) => {
+  console.log("-----------------------------")
+  const participant = await trustshare.api.v1.createParticipant({
+    email: `emailparticipant+${Math.random()}@email.com`
+  })
+
+  const result = await trustshare.api.v1.createVerification({
+    id: participant.api.v1.createParticipant.id,
+  });
+
+  console.log(result.api.v1)
+  console.log({
+    participantSecret: result.api.v1.createVerification.client_secret,
+    participantId: result.api.v1.createVerification.participant.id
+  })
+  res.set('Access-Control-Allow-Origin', req.get('origin'));
+  res.json({
+    participantSecret: result.api.v1.createVerification.client_secret,
+    participantId: result.api.v1.createVerification.participant.id
+  })
+  console.log("-----------------------------\n\n")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
+import createSDK from '@trustshare/sdk';
 import './App.css';
 
+const trustshare = createSDK('sandbox_pk_tYZwAQu221iERYfM0jrCryAGIbNfG7U0');
+
 function App() {
+  const startVerification = async () => {
+
+    console.log("Fetches client secret from the server...");
+    const data = await fetch("http://localhost:4000/get-verification", { method: "GET", mode: 'cors' }).then((resp) => resp.json());
+    console.log("Client secret successfully fetched", data);
+    console.log("Initialising the client SDK...");
+    trustshare.sdk.v1.confirmVerification(data.participantSecret).then((result) => console.log(result));
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <p>
+
+            <button onClick={startVerification}>Start verification</button>
+          </p>
         </p>
         <a
           className="App-link"
