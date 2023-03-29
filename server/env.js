180e60c1b5c04a2b0ed6123f3e410a7e49df10b5
Nikola Medarevic <nikola@petmediagroup.com>
2023-03-29 13:52:39 +0200

    Remove keys

--- server/env.js
+++ server/env.js
@@ -1,3 +1,3 @@
 exports.env = {
-  secretKey: "sandbox_api_xT0LviabyigjDzskQef23eh6lSJkl0digB8Oy8PG7eYRJtHOz9YD7KKVbW6NHlFc"
+  secretKey: ""
 }
--- src/env.js
+++ src/env.js
@@ -1,3 +1,3 @@
 export const env = {
-  publicKey: "sandbox_pk_aOgjaUSEkT9q8J7AZ9o02h6JmY7FjWuu"
+  publicKey: ""
 }
